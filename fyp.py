44bc436e21e3c10c69fd8cc41ca91ee2347f8464
project-714 <zainakram053@gmail.com>
2024-05-12 10:01:55 +0500

    Commit

---
+++ fyp.py
@@ -0,0 +1,40 @@
+from flask import Flask, request,jsonify
+import tensorflow as tf
+import librosa
+import numpy as np
+
+app = Flask(__name__)
+
+model = tf.keras.models.load_model('D:/speech-emotion-recognition-master/models/cnn.keras')
+
+d = {0: 'Happy', 1: 'Sad', 2: 'Angry', 3: 'Neutral', 4: 'Calm', 5: 'Fearful', 6:'Disgust', 7: 'Surprised'}
+
+def func(filename):
+    audio, sample_rate = librosa.load(filename)
+    mfccs_features = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
+    mfccs_scaled_features = np.mean(mfccs_features.T,axis=0)
+    mfccs_scaled_features=mfccs_scaled_features.reshape(1,-1)
+    predicted_label=np.argmax(model.predict(mfccs_scaled_features),axis=1)
+    return d[predicted_label[0]]
+
+@app.route('/', methods=['GET','POST'])
+def predict():
+    if 'audio' not in request.files:
+        return 'No file provided', 400
+
+    audio_file = request.files['audio']
+    if not audio_file.filename.lower().endswith('.wav'):
+        return 'Invalid file type, must be .wav', 400
+    preditction = func(audio_file)
+    print(preditction)
+    return preditction
+
+
+if __name__ == '__main__':
+    # app.run(debug=True ,port=8080,use_reloader=False) 
+    app.run(debug=True)
+
+
+# @app.route("/")
+# def home():
+#     return "Home Function Called"
